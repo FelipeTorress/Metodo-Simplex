e0c6e9251aaa04cfc698e7d66586a19cadfbb3cd
FelipeTorress <felipetoress@gmail.com>
2020-09-27 23:40:00 -0300

    Correção de erros

--- Simplex(utilizando arquivos).py
+++ Simplex(utilizando arquivos).py
@@ -1,12 +1,19 @@
 def encontrarCoeficientes(linha,listaDeCoeficientes):
     indexX = -1
+    count = -1
     for palavra in linha:
-
+            count+=1    
             indexX = -1
             for letra in palavra:
                 indexX+=1
-                if letra == 'X' or letra == 'x': 
-                    listaDeCoeficientes.append(float(palavra[:indexX]))
+                if letra == 'X' or letra == 'x':
+                    if count > 0:
+                        if linha[count - 1] == '-':
+                            listaDeCoeficientes.append(-1*float(palavra[:indexX]))
+                        else:
+                            listaDeCoeficientes.append(float(palavra[:indexX]))
+                    else:
+                        listaDeCoeficientes.append(float(palavra[:indexX]))
                     break
     listaDeCoeficientes.append(float(linha[len(linha)-1]))
 
@@ -77,21 +84,23 @@ def isMaximizar(objetivo):
 def lerArquivo():
     restricao = []
     quantLinhas = 0
-    with open('dados.txt', 'r') as arquivo:
+    with open('dados3.txt', 'r') as arquivo:
         objetivo = arquivo.readline()
         for linha in arquivo.readlines():
             quantLinhas+=1
             restricao.append(linha.split(' '))
     return objetivo, restricao, quantLinhas
 
-def econtrarVariaveis(basicas, naobasica, quantDeLinhas, quantDeVariaveis):
-    for i in range(quantDeVariaveis):
-        if i == 0:
-            continue
-        elif i < quantDeLinhas:
-            naobasica.append('x'+str(i))
-        else:
-            basicas.append('x'+str(i))
+def encontrarVariaveis(basicas, naobasica, quantDeLinhas, quantDeVariaveis): 
+    num = quantDeVariaveis - 1 - quantDeLinhas 
+
+    for i in range(num):
+        naobasica.append('x'+str(i+1))
+
+    num = num + 1
+    for i in range(quantDeLinhas):
+        basicas.append('x'+str(i+num))
+    
     return basicas, naobasica
 
 def solucaoOtima(tableau):
@@ -119,7 +128,8 @@ def encontrarVariavelParaSair(tableau, variaveisBasicas, indexVariavelParaEntrar
     indexParaSair = -1
 
     for i in range(len(tableau) - 1):
-        if tableau[i][indexVariavelParaEntrar] > 0:
+        valor =tableau[i][indexVariavelParaEntrar]
+        if valor > 0:
             valorAtual = tableau[i][len(tableau[i])-1] / tableau[i][indexVariavelParaEntrar]
             if menorValor > valorAtual or menorValor == -1:
                 indexParaSair = i
@@ -148,15 +158,19 @@ def pivoteamento(tableau, indexEntrar, indexSair):
 
 
         elif i != indexSair and tableau[i][indexEntrar] < 0: 
-                pass
+            linha = []
+            aux = tableau[i][indexEntrar] * -1
 
-            
+            for j in range( len(tableau[0]) ):
+                linha.append(tableau[indexSair][j] * aux + tableau[i][j])
+              
+            tableau[i] = linha
+    
     return tableau
 
 def trocarVariaveis(indexEntrar, indexSair, naoBasicas, basicas,tableau):
     aux = naoBasicas[indexEntrar]
     aux2 = basicas[indexSair]
-
     naoBasicas[indexEntrar] = aux2
     basicas[indexSair] = aux
     
@@ -167,38 +181,77 @@ def resolverTableau(tableau, variaveisBasicas, variaveisNaoBasicas):
     while not solucaoOtima(tableau):
         indexVariavelParaEntrar = encontarVariavelParaEntrar(tableau,variaveisNaoBasicas)
         indexVariavelParaSair = encontrarVariavelParaSair(tableau,variaveisBasicas,indexVariavelParaEntrar)
-        tableau = trocarVariaveis(indexVariavelParaEntrar,indexVariavelParaSair, variaveisNaoBasicas, variaveisBasicas, tableau)
+        
+        if indexVariavelParaSair == -1: #sem solução
+            return None
 
+        tableau = trocarVariaveis(indexVariavelParaEntrar,indexVariavelParaSair, variaveisNaoBasicas, variaveisBasicas, tableau)
     return tableau
     
-def resultados(tableau, vetorBasico, vetorNaoBasico):
-    print('Tableau Final:')
+def resultados(tableau, vetorBasico, vetorNaoBasico, objetivo, restricao):
 
-    tableauFinal =[]
+    if tableau == None:
+        print('o Problema possui infinitas soluções')
+    else:
+        print('Problema Inicial: ')
+        print(objetivo)
+        frase = ''
+        for i in range(len(restricao)):
+            frase=''
+            for j in restricao[i]:
+                frase = frase+' '+j
+            print(frase)
 
-    for linha in range(len(tableau)):
-        aux = []
-        for coluna in range(len(tableau[0])):
-            aux.append(round(tableau[linha][coluna], 1))
-        tableauFinal.append(aux)
 
-    for linha in range(len(tableau)):
-        print(tableauFinal[linha])
+        tableauFinal =[]
 
-    print('Variáveis Básicas: '+str(vetorBasico))
-    print('Variáveis Não Básicas: '+str(vetorNaoBasico))
-    print('Valor de Z = '+ str( tableauFinal[len(tableauFinal) - 1] [len(tableauFinal[0])-1 ] ) )
+        for linha in range(len(tableau)):
+            aux = []
+            for coluna in range(len(tableau[0])):
+                aux.append(round(tableau[linha][coluna], 1))
+            tableauFinal.append(aux)
+
+        print('\tTableau Final:')
+
+        for linha in range(len(tableau)):
+            print('   '+str(tableauFinal[linha]))
+
+        print('Variáveis Básicas: '+str(vetorBasico))
+        print('Variáveis Não Básicas: '+str(vetorNaoBasico))
+        print('Valor de Z = '+ str( tableauFinal[len(tableauFinal) - 1] [len(tableauFinal[0])-1 ] ) )
+
+'''def acharBases(tableau):
+    bases = []
+    for c in range(len(tableau[0])-1):
+        aux = 0
+        aux2 = 0
+        for l in range(len(tableau[:-1])):
+            if not tableau[l][c]==0 and not tableau[l][c]==1:
+                aux=-1
+            elif tableau[l][c]==1 and not aux==-1:
+                aux+=1
+                aux2=c
+        if aux==1:
+            bases.append('x'+str(aux2+1))
+    base=[]
+    for l in range(len(tableau)):
+        for n in range(len(tableau[0])):
+            for b in range(len(bases)):
+                if tableau[l][n]==1 and ('x'+str(n+1) in bases[b]):
+                    base.append(bases[b])
+    return base'''
 
 def main():
     tableau = []
-    variaveisBasicas = []
-    variaveisNaoBasicas = []
+    basicas = []
+    naobasicas = []
 
-    objetivo, restricao, quantLinhas = lerArquivo()
+    funccObjetivo, restricao, quantLinhas = lerArquivo()
+    objetivo = funccObjetivo 
     objetivo = isMaximizar(objetivo) # saber se é de maximizar ou minimizar(multiplicando por -1 se for minimizar)
-    tableau = montarTableau(objetivo, restricao, quantLinhas) 
-    basicas, naobasicas = econtrarVariaveis(variaveisBasicas,variaveisNaoBasicas, quantLinhas,len(tableau[0]))
-    tableau = resolverTableau(tableau,variaveisBasicas,variaveisNaoBasicas)
-    resultados(tableau, basicas, naobasicas)
+    tableau = montarTableau(objetivo, restricao, quantLinhas)
+    basicas, naobasicas = encontrarVariaveis(basicas,naobasicas, quantLinhas,len(tableau[0]))
+    tableau = resolverTableau(tableau,basicas,naobasicas)
+    resultados(tableau, basicas, naobasicas, funccObjetivo, restricao)
 
 main()
